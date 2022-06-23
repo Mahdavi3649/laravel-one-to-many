fb30dfcdf0cedfef7061d15ececab5aad3df47d8
Seyedmoslem Mahdavi <95871652+Mahdavi3649@users.noreply.github.com>
2022-06-23 15:45:25 +0200

    added relation posts table & categories table

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
@@ -11,5 +12,15 @@ class Post extends Model
     public static function generateSlug($title)
     {
         return Str::slug($title, '-');
+    }
+
+    /**
+     * Get the category that owns the Post
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
     }
 }
